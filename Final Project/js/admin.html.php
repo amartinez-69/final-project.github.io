3d479141e1e1503bb065f3f6ba10903303592083
Katherine Vogt <kvogt@smith.edu>
2025-04-22 18:42:58 -0400

    starting log in pages

---
+++ Final Project/js/admin.html.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h2>Administrator's Page</h2>
+    <button type="button" onclick="reports()">Show Users</button>
+    <div id="demo">
+    </div>
+
+    <script src="../js/admin.js"></script>
+    </body>
+</html>
---
+++ Final Project/js/admin.js
@@ -0,0 +1,9 @@
+function reports() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+    xhttp.open("GET", "../output/users.JSON");
+    xhttp.send();
+}

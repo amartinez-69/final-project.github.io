3f362572cf7079442f43a61c192cf3582e78ea8f
amartinez-69 <amartinez69@smith.edu>
2025-04-29 19:57:32 -0400

    1

---
+++ Final Project/html/demoPage.html.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>quickjot</title>
+<link rel="stylesheet" href="../green.css">
+</head>
+<body>
+
+<section>
+<h1 stye="float: left;">quickjot</h1>
+<p>
+    <button><a href="login.html">Login</a></button>
+    <button><a href="signup.html">Sign Up</button>
+</p>
+</section>
+
+<section> 
+<nav>
+    <ul>
+        <li class="navbar"><a href="login.html">folders</a></li>
+        <li class="navbar"><a href="login.html">habit tracker</a></li>
+    </ul>
+</nav>
+
+
+<article>
+<table>
+    <tr>
+        <th class="day-header">monday</th>
+        <th>♡︎</th>
+    </tr>
+    <tr>
+      <th>  <div>
+            <div class="planner">
+                <div class="todoheader">
+                    <input type="text" class="todo-input" placeholder="scheduled events">
+                    <span onclick="newElement()" class="addBtn">Add</span>
+                  </div>
+                  <br>
+                  <ul class="myUL">
+                  </ul>
+            </div></th>
+      <th> <div class="planner">
+                <div class="todoheader">
+                    <input type="text" class="todo-input" placeholder="to-dos">
+                    <span onclick="newElement()" class="addBtn">Add</span>
+                  </div>
+                  <br>
+                  <ul class="myUL">
+                  </ul>
+            </div></th>
+      <tr>
+</table>
+<table>
+        <th class="day-header">tuesday</th>
+        <th>♡︎</th>
+      </tr>
+        <th>  <div>
+        <div class="planner">
+            <div class="todoheader">
+                <input type="text" class="todo-input" placeholder="scheduled events">
+                <span onclick="newElement()" class="addBtn">Add</span>
+              </div>
+              <br>
+              <ul class="myUL">
+              </ul>
+        </div></th>
+      <th> <div class="planner">
+                <div class="todoheader">
+                    <input type="text" class="todo-input" placeholder="to-dos">
+                    <span onclick="newElement()" class="addBtn">Add</span>
+                  </div>
+                  <br>
+                  <ul class="myUL">
+                  </ul>
+            </div></th>
+    </tr>
+</table>
+<table>
+    <tr>
+        <th class="day-header">wednesday</th>
+        <th>♡︎</th>
+    </tr>
+    <tr>
+      <th>  <div>
+            <div class="planner">
+                <div class="todoheader">
+                    <input type="text" class="todo-input" placeholder="scheduled events">
+                    <span onclick="newElement()" class="addBtn">Add</span>
+                  </div>
+                  <br>
+                  <ul class="myUL">
+                  </ul>
+            </div></th>
+      <th> <div class="planner">
+                <div class="todoheader">
+                    <input type="text" class="todo-input" placeholder="to-dos">
+                    <span onclick="newElement()" class="addBtn">Add</span>
+                  </div>
+                  <br>
+                  <ul class="myUL">
+                  </ul>
+            </div></th>
+      <tr>
+</table>
+<table>
+        <th class="day-header">thursday</th>
+        <th>♡︎</th>
+      </tr>
+        <th>  <div>
+        <div class="planner">
+            <div class="todoheader">
+                <input type="text" class="todo-input" placeholder="scheduled events">
+                <span onclick="newElement()" class="addBtn">Add</span>
+              </div>
+              <br>
+              <ul class="myUL">
+              </ul>
+        </div></th>
+      <th> <div class="planner">
+                <div class="todoheader">
+                    <input type="text" class="todo-input" placeholder="to-dos">
+                    <span onclick="newElement()" class="addBtn">Add</span>
+                  </div>
+                  <br>
+                  <ul class="myUL">
+                  </ul>
+            </div></th>
+    </tr>
+</table>
+<table>
+    <tr>
+        <th class="day-header">friday</th>
+        <th>♡︎</th>
+    </tr>
+    <tr>
+      <th>  <div>
+            <div class="planner">
+                <div class="todoheader">
+                    <input type="text" class="todo-input" placeholder="scheduled events">
+                    <span onclick="newElement()" class="addBtn">Add</span>
+                  </div>
+                  <br>
+                  <ul class="myUL">
+                  </ul>
+            </div></th>
+      <th> <div class="planner">
+                <div class="todoheader">
+                    <input type="text" class="todo-input" placeholder="to-dos">
+                    <span onclick="newElement()" class="addBtn">Add</span>
+                  </div>
+                  <br>
+                  <ul class="myUL">
+                  </ul>
+            </div></th>
+            <tr>
+</table>
+<table>
+        <th class="day-header">saturday</th>
+        <th>♡︎</th>
+    </tr>
+    <tr>
+      <th>  <div>
+            <div class="planner">
+                <div class="todoheader">
+                    <input type="text" class="todo-input" placeholder="scheduled events">
+                    <span onclick="newElement()" class="addBtn">Add</span>
+                  </div>
+                  <br>
+                  <ul class="myUL">
+                  </ul>
+            </div></th>
+      <th> <div class="planner">
+                <div class="todoheader">
+                    <input type="text" class="todo-input" placeholder="to-dos">
+                    <span onclick="newElement()" class="addBtn">Add</span>
+                  </div>
+                  <br>
+                  <ul class="myUL">
+                  </ul>
+            </div></th>
+            <tr>
+</table>
+<table>
+        <th class="day-header">sunday</th>
+        <th>♡︎</th>
+    </tr>
+    <tr>
+      <th>  <div>
+            <div class="planner">
+                <div class="todoheader">
+                    <input type="text" class="todo-input" placeholder="scheduled events">
+                    <span onclick="newElement()" class="addBtn">Add</span>
+                  </div>
+                  <br>
+                  <ul class="myUL">
+                  </ul>
+            </div></th>
+      <th> <div class="planner">
+                <div class="todoheader">
+                    <input type="text" class="todo-input" placeholder="to-dos">
+                    <span onclick="newElement()" class="addBtn">Add</span>
+                  </div>
+                  <br>
+                  <ul class="myUL">
+                  </ul>
+            </div></th>
+</table>
+</article>
+
+
+
+<script src="../js/todo.js"></script>
+</section>
+
+
+</body>
+</html>

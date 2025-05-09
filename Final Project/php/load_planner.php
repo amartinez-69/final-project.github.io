a5a5b1451755ee9111429a109a8a1f371cb688a4
amartinez-69 <amartinez69@smith.edu>
2025-05-08 20:12:31 -0400

    2

--- Final Project/php/load_planner.php
+++ Final Project/php/load_planner.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['user'])) {
 }
 
 $user = basename($_SESSION['user']);
-$filename = "../user_data/{$user}_planner.json";
+$filename = "../output/user_data/{$user}_planner.json";
 
 if (file_exists($filename)) {
     echo file_get_contents($filename);
